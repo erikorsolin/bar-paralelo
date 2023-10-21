0257bf33e6f3c99773a041b4324f6a0399bcfdc3
Erik Orsolin <107228254+erikorsolin@users.noreply.github.com>
2023-10-20 22:00:40 -0300

    versao 1

--- main.c
+++ main.c
@@ -17,31 +17,70 @@ sem_t sem_pedido, sem_entrega, mutex;
 
 void* cliente(void* arg) {
     while (rodada < R) {
-        usleep(rand() % max_conversa); // conversaComAmigos
-        sem_wait(&sem_pedido); // fazPedido
-        // esperaPedido
-        sem_wait(&sem_entrega); // recebePedido
-        usleep(rand() % max_consumo); // consomePedido
+        // Conversando
+        sleep(rand() % max_conversa);
+
+        // Fazendo o pedido
+        sem_wait(&mutex);
+        
+        if (rodada < R) {
+        sem_post(&sem_pedido);
+        printf("Cliente %ld faz pedido\n", pthread_self());
+        }
+        
+        sem_post(&mutex);
+
+        // Esperando o pedido ser entregue
+        sem_wait(&sem_entrega);
+
+        sem_wait(&mutex);
+        printf("Cliente %ld recebe pedido\n", pthread_self());
+        sem_post(&mutex);
+
+        // Comendo
+        sleep(rand() % max_consumo);
+    
     }
-    return NULL;
+    return 0;
 }
-
-
 
 void* garcom(void* arg) {
     while (rodada < R) {
-        for(int i = 0; i < Gn; i++) {
-            sem_post(&sem_pedido); // recebeMaximoPedidos
+        int rodada_local = rodada;
+
+        // Esperando os pedidos
+        for (int i = 0; i < Gn; i++) {
+            sem_wait(&sem_pedido);
+
+            sem_wait(&mutex);
+            printf("Garçom %ld anota pedido\n", pthread_self());
+            sem_post(&mutex);
         }
-        // registraPedidos
-        for(int i = 0; i < Gn; i++) {
-            sem_post(&sem_entrega); // entregaPedidos
-        }
+
+        // Entregando o pedido
         sem_wait(&mutex);
-        rodada++;
+        for (int i = 0; i < Gn; i++) {
+            sem_post(&sem_entrega);
+            printf("Garçom %ld entrega pedido\n", pthread_self());
+        }
         sem_post(&mutex);
+        
+        // Verificando se a rodada acabou
+        int value_pedidos, value_entregas;
+
+        sem_getvalue(&sem_pedido, &value_pedidos);
+        sem_getvalue(&sem_entrega, &value_entregas);
+
+        sem_wait(&mutex);
+        if (!value_pedidos && !value_entregas && rodada_local == rodada) {
+            printf("\nGarçom %ld terminou a rodada %d\n\n", pthread_self(), (rodada + 1));
+            rodada++;
+        }
+        sem_post(&mutex);
+
     }
-    return NULL;
+    return 0;
+
 }
 
 
@@ -60,5 +99,37 @@ int main(int argc, char* argv[]){
     max_conversa = atoi(argv[5]);
     max_consumo = atoi(argv[6]);
 
+    // Inicializando os semáforos
+    sem_init(&sem_pedido, 0, 0);
+    sem_init(&sem_entrega, 0, 0);
+    sem_init(&mutex, 0, 1);
 
+    // Inicializando as threads
+    pthread_t clientes[N];
+    pthread_t garcons[G];
+
+    // Criando as threads
+    for (int i = 0; i < N; i++) {
+        pthread_create(&clientes[i], NULL, cliente, NULL);
+    }
+
+    for (int i = 0; i < G; i++) {
+        pthread_create(&garcons[i], NULL, garcom, NULL);
+    }
+
+    // Esperando as threads terminarem
+    for (int i = 0; i < N; i++) {
+        pthread_join(clientes[i], NULL);
+    }
+
+    for (int i = 0; i < G; i++) {
+        pthread_join(garcons[i], NULL);
+    }
+
+    // Destruindo os semáforos
+    sem_destroy(&sem_pedido);
+    sem_destroy(&sem_entrega);
+    sem_destroy(&mutex);
+
+    return 0;
 }
