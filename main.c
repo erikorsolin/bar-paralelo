86240e98bd4fa2d8e7147031e83023e2ea88f4df
Erik Orsolin <107228254+erikorsolin@users.noreply.github.com>
2023-10-25 20:00:49 -0300

    arrumada ordem, arrumada formatação, parametrização por estrutura

--- main.c
+++ main.c
@@ -5,17 +5,30 @@
 #include <unistd.h>
 
 
-
 // N -> número de clientes
 // G -> número de garçons
 // Gn -> quantidade de clientes que cada Garçom  atende
 // R -> número de rodadas
 
-int N, G, Gn, R, max_conversa, max_consumo, rodada = 0, pedidos_pendentes = 0;
-sem_t sem_pedidos, sem_entregas, mutex_pedidos, mutex_entregas;
+
+int N, G, Gn, R, max_conversa, max_consumo, rodada = 0, index_pedidos = 0, index_entregas = 0;
+sem_t sem_pedidos, sem_entregas, mutex_pedidos, mutex_entregas, garcons_prontos;
+
+typedef struct {
+    int id_cliente;
+    int* pedidos;
+} info_clientes;
+
+typedef struct {
+    int id_garcom;
+    int* pedidos;
+} info_garcons;
 
 
-void* cliente(void* id_cliente) {
+void* cliente(void* info_cliente) {
+    
+    info_clientes* cliente = (info_clientes*) info_cliente;
+    
     while (rodada < R) {
 
         // Conversando
@@ -23,27 +36,13 @@ void* cliente(void* id_cliente) {
 
         // Fazendo o pedido
         sem_wait(&mutex_pedidos);
-
-        if (rodada < R) {
-
-            pedidos_pendentes++;
-            printf("Cliente %d faz pedido                    ++pedidos pendentes = %d\n", *(int*)id_cliente, pedidos_pendentes);
-            sem_post(&sem_pedidos);
-            
-        } else exit(1);
-
+        printf("CLIENTE %02d PEDIU\n", cliente->id_cliente);
+        cliente->pedidos[(index_pedidos++)%N] = cliente->id_cliente;
+        sem_post(&sem_pedidos);
         sem_post(&mutex_pedidos);
 
         // Esperando o pedido ser entregue
         sem_wait(&sem_entregas);
-
-        // Recebendo o pedido
-        sem_wait(&mutex_pedidos);
-
-        printf("Cliente %d recebe pedido                 --pedidos pendentes = %d\n", *(int*)id_cliente, --pedidos_pendentes);
-        if (!(pedidos_pendentes)) printf("\nFim da rodada %d\n\n", ++rodada);
-
-        sem_post(&mutex_pedidos);
 
         // Comendo
         sleep(rand() % max_consumo);
@@ -52,26 +51,49 @@ void* cliente(void* id_cliente) {
     return 0;
 }
 
-void* garcom(void* id_garcom) {
+
+
+void* garcom(void* info_garcom) {
+
+    info_garcons* garcom = (info_garcons*) info_garcom;
+
+    int anotados[Gn];
+    for (int i = 0; i < Gn; i++) anotados[i] = 0;
+
     while (rodada < R) {
 
-        // Esperando os pedidos
+        // Esperando e anotando os pedidos
         for (int i = 0; i < Gn; i++) {
             sem_wait(&sem_pedidos);
-            printf("Garçom  %d anota %dº pedido\n", *(int*)id_garcom, i + 1);
+            sem_wait(&mutex_entregas);
+            printf("GARÇOM  %02d ANOTOU   O PEDIDO DO CLIENTE %02d (%d/%d)\n", garcom->id_garcom, garcom->pedidos[(index_entregas)%N], i + 1, Gn);
+            anotados[i] = garcom->pedidos[(index_entregas++)%N];
+            sem_post(&mutex_entregas);
         }
         
-        // Entregando o pedido
+        // Entregando os pedidos
+        for (int i = 0; i < Gn; i++) {
+            printf("GARÇOM  %02d ENTREGOU O PEDIDO DO CLIENTE %02d\n", garcom->id_garcom, anotados[i]);
+            sem_post(&sem_entregas);
+        }
+
+        // Esperando todos os garcons
         sem_wait(&mutex_entregas);
+        if (index_entregas % (G * Gn)) {
 
-        printf("Garçom  %d entrega pedidos\n", *(int*)id_garcom);
-        for (int i = 0; i < Gn; i++) sem_post(&sem_entregas);
+            sem_post(&mutex_entregas);
+            sem_wait(&garcons_prontos);
+
+        } else {
+
+            for(int i = 0; i < G - 1; i++) sem_post(&garcons_prontos);
+            printf("\nFim da rodada %d\n\n", ++rodada);
+            sem_post(&mutex_entregas);
+
+        }
         
-        sem_post(&mutex_entregas);
-
     }
     return 0;
-
 }
 
 
@@ -90,39 +112,48 @@ int main(int argc, char* argv[]){
     max_conversa = atoi(argv[5]);
     max_consumo = atoi(argv[6]);
 
+    // Lista de pedidos
+    int* pedidos = (int*) malloc(N * sizeof(int));
+    info_clientes info_cliente[N];
+    info_garcons info_garcom[G];
+
+    for (int i = 0; i < N; i++) pedidos[i] = 0;
+
+    for (int i = 0; i < N; i++) {
+        info_cliente[i].id_cliente = i + 1;
+        info_cliente[i].pedidos = pedidos;
+    }
+
+    for (int i = 0; i < G; i++) {
+        info_garcom[i].id_garcom = i + 1;
+        info_garcom[i].pedidos = pedidos;
+    }
+
     // Inicializando os semáforos
     sem_init(&sem_pedidos, 0, 0);
     sem_init(&sem_entregas, 0, 0);
     sem_init(&mutex_pedidos, 0, 1);
     sem_init(&mutex_entregas, 0, 1);
+    sem_init(&garcons_prontos, 0, 0);
 
     // Inicializando as threads
-    int id_clientes[N], id_garcons[G];
     pthread_t clientes[N];
     pthread_t garcons[G];
 
     // Abrindo o bar
     printf("\nBAR ABERTO: %d RODADAS GRÁTIS!!!\n\n", R);
 
-    // Criando as threads
-    for (int i = 0; i < N; i++) {
-        id_clientes[i] = i + 1;
-        pthread_create(&clientes[i], NULL, cliente, &id_clientes[i]);
-    }
+    // Criando as threads dos clientes
+    for (int i = 0; i < N; i++) pthread_create(&clientes[i], NULL, cliente, (void*)&info_cliente[i]);
 
-    for (int i = 0; i < G; i++) {
-        id_garcons[i] = i + 1;
-        pthread_create(&garcons[i], NULL, garcom, &id_garcons[i]);
-    }
+    // Criando as threads dos garçons
+    for (int i = 0; i < G; i++) pthread_create(&garcons[i], NULL, garcom, (void*)&info_garcom[i]);
 
-    // Esperando as threads terminarem
-    for (int i = 0; i < N; i++) {
-        pthread_join(clientes[i], NULL);
-    }
+    // Esperando as threads clientes terminarem
+    for (int i = 0; i < N; i++) pthread_join(clientes[i], NULL);
 
-    for (int i = 0; i < G; i++) {
-        pthread_join(garcons[i], NULL);
-    }
+    // Esperando as threads garçons terminarem
+    for (int i = 0; i < G; i++) pthread_join(garcons[i], NULL);
 
     // Fechando o bar
     printf("\nBAR FECHADO, ATÉ A PRÓXIMA!!!\n");
