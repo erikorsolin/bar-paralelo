2be9381526b8643eb2aa8be4fcffeb278fd825ae
Rafael <rafael2004.correa@gmail.com>
2023-10-27 14:52:16 -0300

    resolvido problema da lista de mutex

--- main.c
+++ main.c
@@ -16,43 +16,51 @@ sem_t sem_pedidos, sem_entregas, mutex_pedidos, mutex_entregas, garcons_prontos;
 
 typedef struct {
     int id_cliente;
-    int* pedidos;
-    sem_t mutex_cliente;
-    sem_t* lista_mutex_cliente;
+    int* lista_id;
+    sem_t** lista_mutex;
 } info_clientes;
 
 typedef struct {
     int id_garcom;
-    int* pedidos;
-    sem_t* lista_mutex_cliente;
+    int* lista_id;
+    sem_t** lista_mutex;
 } info_garcons;
 
 
 void* cliente(void* info_cliente) {
     
     info_clientes* cliente = (info_clientes*) info_cliente;
+
+    sem_t mutex_cliente;
+    sem_init(&mutex_cliente, 0, 0);
     
     while (rodada < R) {
 
         // Conversando
         sleep(rand() % max_conversa);
 
-        // Fazendo o pedido
+        // Fazendo pedido
         sem_wait(&mutex_pedidos);
-        printf("CLIENTE %02d PEDIU    %p\n", cliente->id_cliente, &cliente->mutex_cliente);
-        cliente->pedidos[(index_pedidos++)%N] = cliente->id_cliente;
+
+        printf("CLIENTE %02d PEDIU\n", cliente->id_cliente);
+        cliente->lista_id[(index_pedidos)%N] = cliente->id_cliente;
+        cliente->lista_mutex[(index_pedidos++)%N] = &mutex_cliente;
         sem_post(&sem_pedidos);
+
         sem_post(&mutex_pedidos);
 
-        // Esperando o pedido ser entregue
-        sem_wait(&sem_entregas);
+        // Esperando pedido
+        sem_wait(&mutex_cliente);
+
+        // Recebendo pedido
+        printf("CLIENTE %02d RECEBEU\n", cliente->id_cliente);
 
         // Comendo
         sleep(rand() % max_consumo);
     
     }
 
-    sem_destroy(&cliente->mutex_cliente);
+    sem_destroy(&mutex_cliente);
     return 0;
 }
 
@@ -62,23 +70,32 @@ void* garcom(void* info_garcom) {
 
     info_garcons* garcom = (info_garcons*) info_garcom;
 
-    int anotados[Gn];
+    int ids_anotados[Gn];
+    sem_t* mutex_anotados[Gn];
 
     while (rodada < R) {
 
-        // Esperando e anotando os pedidos
+        // Enchendo a bandeja
         for (int i = 0; i < Gn; i++) {
+
+            // Esperando pedido
             sem_wait(&sem_pedidos);
+
+            // Anotando pedido
             sem_wait(&mutex_entregas);
-            printf("GARÇOM  %02d ANOTOU   O PEDIDO DO CLIENTE %02d (%d/%d)\n", garcom->id_garcom, garcom->pedidos[(index_entregas)%N], i + 1, Gn);
-            anotados[i] = garcom->pedidos[(index_entregas++)%N];
+
+            printf("GARÇOM  %02d ANOTOU   O PEDIDO DO CLIENTE %02d (%d/%d)\n", garcom->id_garcom, garcom->lista_id[(index_entregas)%N], i + 1, Gn);
+            ids_anotados[i] = garcom->lista_id[(index_entregas)%N];
+            mutex_anotados[i] = garcom->lista_mutex[(index_entregas++)%N];
+
             sem_post(&mutex_entregas);
+            
         }
         
         // Entregando os pedidos
         for (int i = 0; i < Gn; i++) {
-            printf("GARÇOM  %02d ENTREGOU O PEDIDO DO CLIENTE %02d\n", garcom->id_garcom, anotados[i]);
-            sem_post(&sem_entregas);
+            printf("GARÇOM  %02d ENTREGOU O PEDIDO DO CLIENTE %02d\n", garcom->id_garcom, ids_anotados[i]);
+            sem_post(mutex_anotados[i]);
         }
 
         // Esperando todos os garcons
@@ -117,8 +134,8 @@ int main(int argc, char* argv[]){
     max_consumo = atoi(argv[6]);
 
     // Inicializando vetores
-    int* pedidos = (int*) malloc(N * sizeof(int));
-    sem_t* lista_mutex_cliente = (sem_t*) malloc(N * sizeof(sem_t));
+    int* lista_id = (int*) malloc(N * sizeof(int));
+    sem_t** lista_mutex = (sem_t**) malloc(N * sizeof(sem_t*));
     info_clientes info_cliente[N];
     info_garcons info_garcom[G];
 
@@ -139,21 +156,20 @@ int main(int argc, char* argv[]){
     // Criando as threads dos clientes
     for (int i = 0; i < N; i++){
 
-        sem_t mutex_cliente;
-        sem_init(&mutex_cliente, 0, 1);
-
         info_cliente[i].id_cliente = i + 1;
-        info_cliente[i].pedidos = pedidos;
-        info_cliente[i].mutex_cliente = mutex_cliente;
-        info_cliente[i].lista_mutex_cliente = lista_mutex_cliente;
+        info_cliente[i].lista_id = lista_id;
+        info_cliente[i].lista_mutex = lista_mutex;
 
         pthread_create(&clientes[i], NULL, cliente, (void*)&info_cliente[i]);
     }
 
     // Criando as threads dos garçons
     for (int i = 0; i < G; i++){
+
         info_garcom[i].id_garcom = i + 1;
-        info_garcom[i].pedidos = pedidos;
+        info_garcom[i].lista_id = lista_id;
+        info_garcom[i].lista_mutex = lista_mutex;
+
         pthread_create(&garcons[i], NULL, garcom, (void*)&info_garcom[i]);
     }
 
